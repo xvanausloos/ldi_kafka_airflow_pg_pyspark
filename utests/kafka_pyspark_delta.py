b5ff439bccd2898482274f512bec58cb95e7e30e
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-11-12 18:20:02 +0100

    feature: sample pyspark for kafka/delta for unit test mock WIP

--- utests/kafka_pyspark_delta.py
+++ utests/kafka_pyspark_delta.py
@@ -11,25 +11,37 @@ spark = SparkSession.builder.appName("KafkaToDelta").getOrCreate()
 logging.info("Spark session created successfully")
 
 # Read data from Kafka
+topic_name = "topic1"
 kafka_stream = (
     spark.readStream.format("kafka")
     .option("kafka.bootstrap.servers", "kafka:9092")
-    .option("subscribe", "rappel_conso")
+    .option("subscribe", "topic1")
+    .option("startingOffsets", "earliest")
     .load()
 )
-logging.info("Kafka read the stream rappel conso")
+logging.info(f"Kafka read the stream in topic:{topic_name}")
+
+
 
 # Transform the Kafka stream (e.g., extract the value and cast to String)
 kafka_values = kafka_stream.selectExpr("CAST(value AS STRING)")
 
 logging.info(f"Kafka values: {kafka_values}")
-logging.debug(kafka_values.show(truncate=False))
+
+query = kafka_stream.writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .option("truncate", "false") \
+    .start()
+
 
 # Write the transformed stream to a Delta table
-delta_location = "/tmp/delta-table"
-checkpoints_location = "/tmp/checkpoints"
-kafka_values.writeStream.format("delta").outputMode("append").option(
-    "checkpointLocation", checkpoints_location
-).start(delta_location)
+# delta_location = "tmp/delta-table"
+# checkpoints_location = "tmp/checkpoints"
+# kafka_values.writeStream.format("delta").outputMode("append").option(
+#     "checkpointLocation", checkpoints_location
+# ).start("tmp/delta-table")
+#
+# logging.info(f"Stream written in Delta table to {delta_location} ")
 
-logging.info("Stream written in Delta table to {delta_location} ")
+
