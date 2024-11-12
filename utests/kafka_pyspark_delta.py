cad74763a974a705f4500a1fb3fb842a8962a6f8
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-11-12 16:58:33 +0100

    feature: sample pyspark for kafka/delta for unit test mock

--- utests/kafka_pyspark_delta.py
+++ utests/kafka_pyspark_delta.py
@@ -7,18 +7,17 @@ logging.basicConfig(
 )
 
 # Create a Spark session
-spark = SparkSession.builder \
-    .appName("KafkaToDelta") \
-    .getOrCreate()
+spark = SparkSession.builder.appName("KafkaToDelta").getOrCreate()
 
 logging.info("Spark session created successfully")
 
 # Read data from Kafka
-kafka_stream = spark.readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "kafka:9092") \
-    .option("subscribe", "rappel_conso") \
+kafka_stream = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "kafka:9092")
+    .option("subscribe", "rappel_conso")
     .load()
+)
 logging.info("Kafka read the stream rappel conso")
 
 # Transform the Kafka stream (e.g., extract the value and cast to String)
@@ -30,10 +29,8 @@ logging.debug(kafka_values.show(truncate=False))
 # Write the transformed stream to a Delta table
 delta_location = "/tmp/delta-table"
 checkpoints_location = "/tmp/checkpoints"
-kafka_values.writeStream \
-    .format("delta") \
-    .outputMode("append") \
-    .option("checkpointLocation", checkpoints_location) \
-    .start(delta_location)
+kafka_values.writeStream.format("delta").outputMode("append").option(
+    "checkpointLocation", checkpoints_location
+).start(delta_location)
 
 logging.info("Stream written in Delta table to {delta_location} ")
--- utests/pyspark_x.py
+++ utests/pyspark_x.py
@@ -2,15 +2,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
 # Create a Spark session
-spark = SparkSession.builder \
-    .appName("PySpark x") \
-    .getOrCreate()
+spark = SparkSession.builder.appName("PySpark x").getOrCreate()
 
 csv_file = "../data/sample.csv"
 
-df = spark.read \
-    .option("header", "true") \
-    .option("inferSchema", "true") \
-    .csv(csv_file)
+df = spark.read.option("header", "true").option("inferSchema", "true").csv(csv_file)
 
 print(df.show(truncate=False))
